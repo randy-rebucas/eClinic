e3fcf97c948036a6aedf682b91bba78c8bb63a91
randyr <rebucasrandy1986@gmail.com>
2022-03-21 09:05:21 +0800

    update

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -1,10 +1,18 @@
 <div>
-
-<livewire:tables.usertable
+    <livewire:datatable
+    model="App\Models\User"
+    name="users"
+    searchable="name"
+    exclude="updated_at"
+    hidden="email_verified_at"
+    hideable="select"
+    permission="users-create"
+/>
+{{-- <livewire:tables.usertable
     table="user"
     earchable="name"
     exclude="updated_at"
     hidden="email_verified_at"
     hideable="select"
-    permission="users-create"/>
+    permission="users-create"/> --}}
 </div>
